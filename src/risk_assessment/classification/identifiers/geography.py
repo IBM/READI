21a0df4e4747fe77dc913fea15af9b931693597e
Stefano Braghin <527806+stefano81@users.noreply.github.com>
2026-05-17 10:57:38 +0100

    fix: typo
    Signed-off-by: Stefano Braghin <527806+stefano81@users.noreply.github.com>

--- src/risk_assessment/classification/identifiers/geography.py
+++ src/risk_assessment/classification/identifiers/geography.py
@@ -16,19 +16,6 @@ from risk_assessment.classification.identifiers import DictionaryIdentifier, Ide
 logger = logging.getLogger(__name__)
 
 
-def _extract_all_langugage_city_names(file: str) -> list[str]:
-    """Extract city names from a multi-language file.
-
-    Args:
-        file: Path to the file containing city names.
-
-    Returns:
-        List of city names.
-    """
-    with (Path(__file__).parent / file).open("r") as stream:
-        return [line.strip() for line in stream.readlines()]
-
-
 def _extract_city_names(file: str) -> list[str]:
     """Extract city names from a CSV file.
 
@@ -683,7 +670,7 @@ class UnitedStateState(DictionaryIdentifier):
                 "GU",
                 "Guam",
                 "VI",
-                "Vigin Islands",
+                "Virgin Islands",
                 "PR",
                 "Puerto Rico",
                 "FM",
